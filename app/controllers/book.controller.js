b67ef010628da553e6e6215c76e47710c8d15cb5
fitrifityanto <fitrifityanto@gmail.com>
2024-11-21 17:02:04 +0700

    add: proses delete dan ubah

--- app/controllers/book.controller.js
+++ app/controllers/book.controller.js
@@ -24,6 +24,7 @@ const findAll = async (req, res) => {
     res.send(books)
 }
 
+// menampilkan data buku berdasarkan id
 const findBookById = async (req, res) => {
     try {
         const book = await Book.findById(req.params.id)
@@ -31,9 +32,50 @@ const findBookById = async (req, res) => {
             res.status(404).send({ message: `data buku id ${req.params.id} tidak ada` })
         } else res.send(book)
     } catch (error) {
-        res.status(500).send({ message: "error menerima data"})
+        res.status(500).send({ message: "error menerima data" })
     }
     
 }
 
-export { create, findAll, findBookById }
+// proses menghapus 1 data buku
+const deleteBook = async (req, res) => {
+    try {
+        const book = await Book.findByIdAndDelete(req.params.id)
+        if (!book) {
+            res.status(404).send({ message: `data buku id ${req.params.id} gagal di hapus, mungkin data tidak ada` })
+        } else {
+            res.send({ message: 'data buku berhasil dihapus.' })
+        }
+    } catch (error) {
+        res.status(500).send({ message: "data buku tidak bisa dihapus" })
+    }
+}
+
+// proses menghapus semua data buku
+const deleteAll = async (req, res) => {
+    try {
+        const book = await Book.deleteMany({})
+        res.send({ message: `${book.deletedCount} buku berhasil dihapus!` })
+    } catch (error) {
+        res.status(500).send({ message: error.message || 'gagal menghapus semua data buku' })
+    }
+}
+
+// proses update data buku berdasarkan id
+const updateBook = async (req, res) => {
+    try {        
+        // validasi data tidak boleh kosong
+        if (!req.body) {
+            res.status(400).send({ message: "data yang akan diubah tidak boleh kosoong" })
+        }
+        
+        const book = await Book.findByIdAndUpdate(req.params.id, req.body)
+        if (!book) {
+            res.status(404).send({ message: "gagal mengubah data buku, mungkin data buku tidak ada" })
+        } else res.send({ message: "data buku berhasil diubah" })
+    } catch (error) {
+        res.status(500).send({ message: "error mengubah data" })
+    }
+}
+
+export { create, findAll, findBookById, deleteBook, deleteAll, updateBook }
--- app/routes/book.router.js
+++ app/routes/book.router.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { create, findAll, findBookById } from '../controllers/book.controller.js';
+import { create, findAll, findBookById, deleteBook, deleteAll, updateBook } from '../controllers/book.controller.js';
 
 export const router = express.Router()
 
 router.post('/', create);
-router.get('/', findAll)
-router.get('/:id', findBookById)
+router.get('/', findAll);
+router.get('/:id', findBookById);
+router.delete('/:id', deleteBook);
+router.delete('/', deleteAll);
+router.put('/:id', updateBook);
 
