0ccf59133ef5b04be79242b1fd6e5680d10cc1a7
fitrifityanto <fitrifityanto@gmail.com>
2024-11-23 06:01:18 +0700

    mencoba fix db connection

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,6 +4,6 @@ import 'dotenv/config';
 // jika mengakses mongodb local:
 // mongoose.connect('mongodb://127.0.0.1:27017/belajar')
 
-mongoose.connect(process.env.uri, {dbName: 'belajar'})
+mongoose.connect(process.env.uri, { dbName: 'belajar', family: 4, socketTimeoutMS: 1000 })
 .then(() => { console.log('koneksi ke atlas cluster berhasil') })
 .catch(err => console.log('koneksi mongodb bermasalah'))
